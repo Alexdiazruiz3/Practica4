93a0ebbeec544304ebeb5bf54696fe76e11ca293
alexdiazruiz3 <Alexdiazruiz3@gmail.com>
2019-01-22 18:54:03 +0100

    Esta clase java fue creada para declarar el grupo de checkbox y los propios checkbox, que debemos de utilizar para crear uno de los fragment solicitados en el segundo apartado, en este caso en concreto, el de ciudades.

--- app/src/main/java/com/example/alex/practica4/FragmentCiudades.java
+++ app/src/main/java/com/example/alex/practica4/FragmentCiudades.java
@@ -7,106 +7,49 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FragmentCiudades.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FragmentCiudades#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentCiudades extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    RadioGroup grpCiudades;
+    public static  RadioGroup grpCiudades;
     View v;
+    public static RadioButton radioMadrid, radioParis, radioLondes,
+            radioRoma, radioBerlin, radioLisboa, radioBruselas, radioAtenas, radioAmsterdan, radioMoscu;
 
     private OnFragmentInteractionListener mListener;
 
-    public FragmentCiudades() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentCiudades.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentCiudades newInstance(String param1, String param2) {
-        FragmentCiudades fragment = new FragmentCiudades();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_ciudades, container, false);
-    }
+        v = inflater.inflate(R.layout.fragment_fragment_ciudades, container, false);
+        grpCiudades = v.findViewById(R.id.grpCiudades);
+        radioMadrid = v.findViewById(R.id.radioMadrid);
+        radioParis = v.findViewById(R.id.radioParis);
+        radioLondes = v.findViewById(R.id.radioLondres);
+        radioRoma = v.findViewById(R.id.radioRoma);
+        radioBerlin = v.findViewById(R.id.radioBerlin);
+        radioLisboa = v.findViewById(R.id.radioLisboa);
+        radioBruselas = v.findViewById(R.id.radioBruselas);
+        radioAtenas = v.findViewById(R.id.radioAtenas);
+        radioAmsterdan = v.findViewById(R.id.radioAmsterdan);
+        radioMoscu = v.findViewById(R.id.radioMoscu);
+        grpCiudades.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+                String capital = ((RadioButton)v.findViewById(grpCiudades.getCheckedRadioButtonId())).getText().toString();
+                TextView r = getActivity().findViewById(R.id.lblResultado1);
+                r.setText(capital);
+            }
+        });
+            return v;
         }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+        public interface OnFragmentInteractionListener {
+            // TODO: Update argument type and name
+            void onFragmentInteraction(Uri uri);
         }
-    }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+
 }
